626e7d0c6d256ee05e61b37d5d3e255fca0fda3e
Guominyingxiongququ <yxy1106@gmail.com>
2016-04-22 10:46:18 +0200

    change the format of result

--- show_result.m
+++ show_result.m
@@ -19,23 +19,23 @@ function show_result(conf_set)
     fprintf(fid, '<TR>');
     fprintf(fid, '<TD>%s</TD>', 'average PSNR');
     for i = 1:2
-        fprintf(fid, '<TD>%s</TD>', mean(conf_set{i}.scores));
+        fprintf(fid, '<TD>%6.2f</TD>', mean(conf_set{i}.scores(:,3)));
     end
     fprintf(fid, '</TR>\n');
     fprintf(fid, '<TR>');
     fprintf(fid, '<TD>%s</TD>', 'COV PSNR');
     for i = 1:2
-        fprintf(fid, '<TD>%s</TD>', cov(conf_set{i}.scores));
+        fprintf(fid, '<TD>%6.2f</TD>', cov(conf_set{i}.scores(:,3)));
     end
     fprintf(fid, '</TR>\n');
     fprintf(fid, '<TR>');
     fprintf(fid, '<TD>%s</TD>', 'patch_num');
     for i = 1:2
-        fprintf(fid, '<TD>%s</TD>', conf_set{i}.patch_num);
+        fprintf(fid, '<TD>%5d</TD>', conf_set{i}.patch_num);
     end
     fprintf(fid, '</TR>\n');
     
-    fprintf(fid, '<H1>Simulation parameters</H1>\n<TABLE border="1">\n');
+%     fprintf(fid, '<H1>Simulation parameters</H1>\n<TABLE border="1">\n');
     fprintf(fid, sprintf('<TR><TD>Scaling factor<TD>x%d</TR>\n', conf_set{1}.scale));
     fprintf(fid, sprintf('<TR><TD>patch num<TD>x%d</TR>\n', conf_set{1}.patch_num));
     fprintf(fid, '</TABLE>\n');
